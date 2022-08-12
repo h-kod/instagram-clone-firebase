593be3363a12b6531554ccb51528a56936770524
huseyin.emecen <huseyin.emecen@dia.com.tr>
2022-08-12 16:44:45 +0300

    firebase install and connect

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "./router";
 import store from "./store";
+import 'bootstrap';
+import { initializeApp } from "firebase/app";
+
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDD-ZLG_J3aYSvWbBIoUN2UXb88Hzzui6c",
+  authDomain: "instagram-clone-82c30.firebaseapp.com",
+  projectId: "instagram-clone-82c30",
+  storageBucket: "instagram-clone-82c30.appspot.com",
+  messagingSenderId: "371981608268",
+  appId: "1:371981608268:web:229705ee33e734e37c1be9",
+  measurementId: "G-NB31N2WZ1Q"
+};
+
+const fire = initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
